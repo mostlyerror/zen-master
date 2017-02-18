f09233c38e1bb9e4d83448a307b81da72608c1e8
Sarah Chen <SPC@sarahs-air-2.hsd1.tx.comcast.net>
2017-02-18 17:51:50 -0600

    Retrieving statsSummary manifest from riot api

--- riot.js
+++ riot.js
@@ -1,22 +1,43 @@
 const request = require('superagent');
-
+const util = require('util');
 const apiKey = '98b67977-fba4-4435-88a0-461acf65bb34';
 
+const riotApi = {
+  urls: {
+    summonerByName: (name) => {
+      return `https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/${name}?api_key=${apiKey}`;
+    },
+    summonerStatsById: (id, season='SEASON2017') => {
+return `https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/${id}/summary?season=${season}&&api_key=${apiKey}`;
+    },
+  }
+};
 
-// TODO : accept this from the command line?
-const summonerName = 'jasonwaterfallz';
+const name = 'jasonwaterfallz';
 
+// request.get(riotApi.urls.summonerByName(name));
 
-// find the current game that the summoner is in
-const url = `https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/${summonerName}?api_key=${apiKey}`;
-
-
-console.log('requesting ' + url);
+// get summoner data object from riot
+let url = riotApi.urls.summonerByName(name);
 request
   .get(url)
-  .set('Accept', 'application/json')
-  .end(function(err, res){
-    let summonerData = JSON.parse(res.req.res.text);
-    console.log(summonerData);
+  .then(function(res, err){
+    const json = res.res.req.res.text;
+    const parsed = JSON.parse(json);
+    return parsed[name]['id'];
+  })
+    // get the summoner stats summary from riot
+  .then(function(id) {
+    let url = riotApi.urls.summonerStatsById(id);
+    request
+      .get(url)
+      .then(function(res, err) {
+        const json = res.res.req.res.text;
+        const parsed = JSON.parse(json);
+	console.log(util.inspect(parsed));
+      })
+  })
+  .catch(function (err) {
+    console.log('error happened?', err);
   });
 
